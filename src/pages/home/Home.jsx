718d716f5253791723c76bd26cae04533d53e57d
SohamShedale <shedalesoham@gmail.com>
2024-07-01 19:16:50 +0530

    add filtered products

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import Filter from "../../components/filter/Filter";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Layout from "../../components/layout/Layout";
+import ProductCard from "../../components/productCard/ProductCard";
 
 const Home = () => {
   return (
     <Layout>
       <HeroSection/>
       <Filter/>
+      <ProductCard/>
     </Layout>
   );
 };
