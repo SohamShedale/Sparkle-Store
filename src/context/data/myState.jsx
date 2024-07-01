b482b39cd71f80c270637fa0a6f77b787efa63cb
SohamShedale <shedalesoham@gmail.com>
2024-07-01 18:24:14 +0530

    change some code

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react"
 import MyContext from "./myContext"
 
 const myState = ({children}) => {
-    const data = {
-    }
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = () => {
+    let changedTheme = mode == 'light' ? 'dark' : 'light'
+    setMode(changedTheme) 
+    document.body.style.backgroundColor = changedTheme == 'light' ? '#ffffff' : "rgb(17,24,39)";
+    // if(mode == 'light'){
+    //   setMode('dark')
+    //   document.body.style.backgroundColor = "rgb(17,24,39)";
+    // }
+    // else{
+    //   setMode('light')
+    //   document.body.style.backgroundColor = "#ffffff";
+    // }
+  }
+
   return (
-    <MyContext.Provider value={data}>
+    <MyContext.Provider value={{mode,toggleMode}}>
       {children}
     </MyContext.Provider>
   )
