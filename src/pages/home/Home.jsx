b753c952bfa125367c734495e39929bd7e930318
SohamShedale <shedalesoham@gmail.com>
2024-07-03 13:03:51 +0530

    add components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import Filter from "../../components/filter/Filter";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Layout from "../../components/layout/Layout";
 import ProductCard from "../../components/productCard/ProductCard";
+import Testimonial from "../../components/testimonial/Testimonial";
+import Track from "../../components/track/Track";
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <HeroSection/>
       <Filter/>
       <ProductCard/>
+      <Track/>
+      <Testimonial/>
     </Layout>
   );
 };
