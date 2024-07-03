dd9600b3086d8f8f0af6de6e2264bb161d370a25
SohamShedale <shedalesoham@gmail.com>
2024-07-03 14:07:44 +0530

    remove extra lines of code

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -18,7 +18,7 @@ export default function Modal() {
         <button
           type="button"
           onClick={openModal}
-          className="w-full py-2 text-center rounded-lg text-white font-bold bg-green-600"
+          className="w-full py-2 text-center rounded-lg text-white font-bold bg-green-600 hover:bg-violet-600"
         >
           Buy Now
         </button>
@@ -119,7 +119,7 @@ export default function Modal() {
                           <button
                             onClick={closeModal}
                             type="button"
-                            className="focus:outline-none w-full text-white bg-violet-600 bg-green-600 hover:bg-violet-800  outline-0 font-medium rounded-lg text-sm px-5 py-2.5 "
+                            className="focus:outline-none w-full text-white bg-green-600 hover:bg-violet-800  outline-0 font-medium rounded-lg text-sm px-5 py-2.5 "
                           >
                             Order Now
                           </button>
